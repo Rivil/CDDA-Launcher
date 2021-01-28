45e92d4efa5bef3f26aff6dc376b37d93ac3f302
rivil <michal@rivil.co.uk>
2021-01-28 08:24:19 +0000

    First Commit

---
+++ CDDA-Launcher/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CDDA_Launcher.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
